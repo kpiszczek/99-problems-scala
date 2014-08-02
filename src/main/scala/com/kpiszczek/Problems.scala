a5e785c7447f5974cfa6d554268277995435a900
kpiszczek <jppisczek@gmail.com>
2014-08-03 00:06:33 +0200

    initial

---
+++ src/main/scala/com/kpiszczek/Problems.scala
@@ -0,0 +1,71 @@
+package com.kpiszczek
+
+import scala.annotation.tailrec
+
+object Problems {
+  @tailrec
+  def last[A](as: List[A]): Option[A] = as match {
+  	case Nil => None
+  	case a :: Nil => Some(a)
+  	case _ :: tail => last(tail)
+  }
+
+  @tailrec
+  def penultimate[A](as: List[A]): Option[A] = as match {
+  	case Nil | _ :: Nil => None
+  	case a :: _ :: Nil => Some(a)
+  	case _ :: tail => penultimate(tail)
+  }
+
+  @tailrec
+  def nth[A](n: Int, as: List[A]): Option[A] = (n, as) match {
+  	case (_, Nil) => None
+  	case (0, a :: _) => Some(a)
+  	case (n, _ :: tail) => nth(n - 1, tail)
+  }
+
+  def length[A](as: List[A]): Int = 
+    as.foldLeft(0)((acc: Int, _) => acc + 1)
+
+  def reverse[A](as: List[A]): List[A] = 
+  	as.foldLeft(Nil: List[A])((acc: List[A], a: A) => a :: acc)
+
+  def isPalindrome[A](as: List[A]): Boolean = 
+    as == reverse(as)
+
+  def flatten(as: List[_]) : List[_] = as flatMap {
+    case a : List[_] => flatten(a)
+    case a => List(a)
+  }
+
+  def compress[A](as: List[A]): List[A] = {
+  	@tailrec
+    def go(acc: List[A], last: A, as: List[A]): List[A] = as match {
+      case Nil => last :: acc
+      case a :: rest => 
+      	if (a == last) go(acc, last, rest)
+      	else go(last :: acc, a, rest)
+    }
+    as match {
+      case Nil => Nil
+      case a :: rest => reverse(go(Nil, a, rest))
+    }
+  }
+
+  def pack[A](as: List[A]): List[List[A]] = {
+  	@tailrec
+    def go(acc: List[List[A]], last: List[A], as: List[A]): List[List[A]] = as match {
+      case Nil => last :: acc
+      case a :: rest => 
+      	if (a == last.head) go(acc, a :: last, rest)
+      	else go(last :: acc, List(a), rest)
+    }
+    as match {
+      case Nil => Nil
+      case a :: rest => reverse(go(Nil, List(a), rest))
+    }
+  }
+
+  def encode[A](as: List[A]): List[(Int, A)] = 
+    pack(as) flatMap ((a: List[A]) => List((length(a), a.head)))
+}
---
+++ src/test/scala/ProblemsSpec.scala
@@ -0,0 +1,80 @@
+import org.scalatest._
+
+import com.kpiszczek.Problems
+
+class ProblemsSpec extends FlatSpec with Matchers {
+  "1: last" should "return None if list is empty" in {
+    Problems.last[Any](Nil) should be (None)
+  }
+  it should "return last element of non-empty list" in {
+  	Problems.last(1 :: 2 :: 3 :: Nil) should be (Some(3))
+  	Problems.last(List(1, 1, 2, 3, 5, 8)) should be (Some(8))
+  	Problems.last(1 :: Nil) should be (Some(1))
+  }
+
+  "2: penultimate" should "return None if list is empty" in {
+    Problems.penultimate[Any](Nil) should be (None)
+  }
+  it should "return pre-last element of non-empty list" in {
+  	Problems.penultimate(1 :: 2 :: 3 :: Nil) should be (Some(2))
+  	Problems.penultimate(List(1, 1, 2, 3, 5, 8)) should be (Some(5))
+  }
+  it should "return None if list has only one element" in {
+  	Problems.penultimate(1 :: Nil) should be (None)
+  }
+
+  "3: nth" should "return None if list is empty" in {
+    Problems.penultimate[Any](Nil) should be (None)
+  }
+  it should "return nth element of non-empty list" in {
+  	Problems.nth(0, 1 :: 2 :: 3 :: Nil) should be (Some(1))
+  	Problems.nth(2, List(1, 1, 2, 3, 5, 8)) should be (Some(2))
+  }
+  it should "return None if list in shorter than n-elements" in {
+  	Problems.nth(2, 1 :: Nil) should be (None)
+  }
+
+  "4: length" should "count number of elements in a list" in {
+  	Problems.length[Any](Nil) should be (0)
+  	Problems.length("hello" :: Nil) should be (1)
+  	Problems.length(List(1, 1, 2, 3, 5, 8)) should be (6)
+  }
+
+  "5: reverse" should "make a reversed list" in {
+  	Problems.reverse[Any](Nil) should be (Nil)
+  	Problems.reverse("hello" :: Nil) should be (List("hello"))
+  	Problems.reverse(List(1, 1, 2, 3, 5, 8)) should be (List(8, 5, 3, 2, 1, 1))
+  }
+
+  "6: isPalindrome" should "check if given list is a palindrome" in {
+  	Problems.isPalindrome[Any](Nil) should be (true)
+  	Problems.isPalindrome("hello" :: Nil) should be (true)
+  	Problems.isPalindrome(List(1, 1, 2, 3, 5, 8)) should be (false)
+  	Problems.isPalindrome(List(1, 2, 3, 2, 1)) should be (true)
+  }
+
+  "7: flatten" should "flatten a nested list structure" in {
+  	Problems.flatten(Nil: List[Any]) should be (Nil)
+  	Problems.flatten("hello" :: Nil) should be (List("hello"))
+  	Problems.flatten(List(List(1, 1), 2, List(3, List(5, 8)))) should be (List(1, 1, 2, 3, 5, 8))
+  }
+
+  "8: compress" should "eliminate consecutive duplicates of list elements" in {
+  	Problems.compress[Any](Nil) should be (Nil)
+  	Problems.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be (List('a, 'b, 'c, 'a, 'd, 'e))
+  	Problems.compress(List("hello")) should be (List("hello"))
+  	Problems.compress(List(1, 1, 2, 3, 5, 8)) should be (List(1, 2, 3, 5, 8))
+  }
+
+  "9: pack" should "pack consecutive duplicates of list elements into sublists" in {
+  	Problems.pack[Any](Nil) should be (Nil)
+  	Problems.pack(List("hello")) should be (List(List("hello")))
+  	Problems.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be (List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+  }
+
+  "10: encode" should "run-length encoding of a list" in {
+  	Problems.encode[Any](Nil) should be (Nil)
+  	Problems.encode(List("hello")) should be (List((1, "hello")))
+  	Problems.encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be (List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
+  }
+}
